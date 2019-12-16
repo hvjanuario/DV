5543871c50e0c45727fcbd85cba892d69d037c78
hvjanuario <46404498+hvjanuario@users.noreply.github.com>
2019-12-16 12:27:26 +0000

    Update VDS_project.py

--- VDS_project.py
+++ VDS_project.py
@@ -33,50 +33,151 @@ league_options = [dict(label=league, value=league)
 
 ######################################################APP###############################################################
 
+### Option 1 (class one)
 app = dash.Dash(__name__)
+
 app.layout = html.Div([
+    html.Div([
+        html.H1('Emissions Title')
 
-                html.Div([
-                    html.H1('Project Title')
-                ], classname='row1'),
+    ], className='container row 1'),
 
-                html.Div([
-                    html.Div([],classname='')
+    html.Div([
 
-                ], classname='row2')
+        html.Div([
 
-            ])
+            #left hand side little selector box
 
-app = dash.Dash(__name__)
-app.layout = html.Div([
-                #title area
-                html.Div([
-                    html.H1('Project Title')
-                ], className='row1'),
+        ], className='pretty container col1'),
 
-                html.Div([
-                    html.Div([
-                        html.Div([],className=''),
-                        html.Div([],className=''),
-                        html.Div([],className='')
-                    ], className='col_side'), #put selectors here
+        html.Div([
+            html.Div([
+                #little boxes up top
+                html.Div([], className='pretty col3'),
+                html.Div([], className='pretty col3'),
+                html.Div([], className='pretty col3'),
+                html.Div([], className='pretty col3'),
+                html.Div([], className='pretty col3')
 
-                    html.Div([
-                        html.Div([
-                            html.Div([], className='col3'),
-                            html.Div([], className='col3'),
-                            html.Div([], className='col3'),
-                            html.Div([], className='col3'),
-                            html.Div([], className='col3')
+            ], className=' row 1'),
 
-                        ], className=' row_1'),
+            html.Div([
 
-                        html.Div([], className='pretty')
+                #first graph
 
-                    ], className=' col2')
-                ], className='row2'),
+            ], className='pretty')
 
-            ])
+        ], className=' col2')
+
+    ], className=' row 2'),
+
+    html.Div([
+        #last row of graphs
+        html.Div([], className='pretty column'),
+
+        html.Div([], className='pretty column')
+
+    ], className=' row 3')
+])
+
+
+
+
+#Option 2 (uber rides thing)
+app.layout = html.Div(
+    children=[
+        html.Div(
+            className="row",
+            children=[
+                # Column for user controls
+                html.Div(
+                    className="four columns div-user-controls",
+                    children=[
+                        html.H2("DASH - UBER DATA APP"),
+                        html.P(
+                            """Select different days using the date picker or by selecting
+                            different time frames on the histogram."""
+                        ),
+                        html.Div(
+                            className="div-for-dropdown",
+                            children=[
+                                dcc.DatePickerSingle(
+                                    id="date-picker",
+                                    min_date_allowed=dt(2014, 4, 1),
+                                    max_date_allowed=dt(2014, 9, 30),
+                                    initial_visible_month=dt(2014, 4, 1),
+                                    date=dt(2014, 4, 1).date(),
+                                    display_format="MMMM D, YYYY",
+                                    style={"border": "0px solid black"},
+                                )
+                            ],
+                        ),
+                        # Change to side-by-side for mobile layout
+                        html.Div(
+                            className="row",
+                            children=[
+                                html.Div(
+                                    className="div-for-dropdown",
+                                    children=[
+                                        # Dropdown for locations on map
+                                        dcc.Dropdown(
+                                            id="location-dropdown",
+                                            options=[
+                                                {"label": i, "value": i}
+                                                for i in list_of_locations
+                                            ],
+                                            placeholder="Select a location",
+                                        )
+                                    ],
+                                ),
+                                html.Div(
+                                    className="div-for-dropdown",
+                                    children=[
+                                        # Dropdown to select times
+                                        dcc.Dropdown(
+                                            id="bar-selector",
+                                            options=[
+                                                {
+                                                    "label": str(n) + ":00",
+                                                    "value": str(n),
+                                                }
+                                                for n in range(24)
+                                            ],
+                                            multi=True,
+                                            placeholder="Select certain hours",
+                                        )
+                                    ],
+                                ),
+                            ],
+                        ),
+                        html.P(id="total-rides"),
+                        html.P(id="total-rides-selection"),
+                        html.P(id="date-value"),
+                        dcc.Markdown(
+                            children=[
+                                "Source: [FiveThirtyEight](https://github.com/fivethirtyeight/uber-tlc-foil-response/tree/master/uber-trip-data)"
+                            ]
+                        ),
+                    ],
+                ),
+                # Column for app graphs and plots
+                html.Div(
+                    className="eight columns div-for-charts bg-grey",
+                    children=[
+                        dcc.Graph(id="map-graph"),
+                        html.Div(
+                            className="text-padding",
+                            children=[
+                                "Select any of the bars on the histogram to section data by time."
+                            ],
+                        ),
+                        dcc.Graph(id="histogram"),
+                    ],
+                ),
+            ],
+        )
+    ]
+)
 
 ######################################################Callbacks#########################################################
 
